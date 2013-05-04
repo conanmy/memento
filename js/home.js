ee280134805065b2bbf6d6fc81759325b01f9d0f
conanmy <conanmy@yahoo.cn>
2013-05-05 00:18:22 +0800

    将when声明后移，将getMemento变成函数字面量

--- js/home.js
+++ js/home.js
@@ -50,12 +50,6 @@ $(document).ready(function() {
     });
     //请求数据
     $("#target_confirm").bind("click", function() {
-        $.when(getMyInfo, getFriendInfo).then(
-            getMemento, 
-            function(){
-                alert('用户信息异常');
-            }
-        );
         
         var target = $("#target").val();
         //根据用户输入获取目标好友信息
@@ -76,39 +70,48 @@ $(document).ready(function() {
         });
         
         //请求并处理时间线数据
-        var getMemento = $.get("cal_memento.php", {
-            'key' : key,
-            'uid' : Memento.info.uid,
-            'uname' : Memento.info.uname,
-            'targetid' : Memento.info.targetid,
-            'targetname' : Memento.info.targetname
-        }, function(data) {
-            var index = data.indexOf('[{"uid":');
-            if (index < 0) {
-                alert("网络状况不佳，请重试");
-                //隐藏加载提示
-                $("#jTemplatesTest").html("");
-            } else {
-                data = data.slice(index);
-                data = $.parseJSON(data);
-                $("#jTemplatesTest").setTemplateElement("template");
-                $("#jTemplatesTest").processTemplate(data);
-                //设置iframe高度
-                XN_RequireFeatures(["Connect", "CanvasUtil"], function() {
-                    //这里要自己填两个参数：api_key和跨域文件xd_receiver.html的路径
-                    XN.Main.init("3ada89292ed94a61be1ac634b3291750", "xd_receiver.html");
-                    //这里要填需要自定义的高度如"800px"，注意应该有'px'单位
-                    var height_frame = document.getElementById("container").scrollHeight + 100;
-                    XN.CanvasClient.setCanvasHeight(height_frame + "px");
-                });
-                //生成浮动层
-                $("<div id='floatblock'><ul><li><img src='" + Memento.info.uimg + "' /></li><li>" + Memento.info.uname + "</li><li class='light'>|</li><li>" + Memento.info.targetname + "</li><li><img src='" + Memento.info.targetimg + "' /></li></ul></div>").appendTo("#container");
-                //隐藏输入框
-                $(".input_area").hide();
-                //显示“生成时间线”按钮
-                $("#tools").show();
+        var getMemento = function(){
+            $.get("cal_memento.php", {
+                'key' : key,
+                'uid' : Memento.info.uid,
+                'uname' : Memento.info.uname,
+                'targetid' : Memento.info.targetid,
+                'targetname' : Memento.info.targetname
+            }, function(data) {
+                var index = data.indexOf('[{"uid":');
+                if (index < 0) {
+                    alert("网络状况不佳，请重试");
+                    //隐藏加载提示
+                    $("#jTemplatesTest").html("");
+                } else {
+                    data = data.slice(index);
+                    data = $.parseJSON(data);
+                    $("#jTemplatesTest").setTemplateElement("template");
+                    $("#jTemplatesTest").processTemplate(data);
+                    //设置iframe高度
+                    XN_RequireFeatures(["Connect", "CanvasUtil"], function() {
+                        //这里要自己填两个参数：api_key和跨域文件xd_receiver.html的路径
+                        XN.Main.init("3ada89292ed94a61be1ac634b3291750", "xd_receiver.html");
+                        //这里要填需要自定义的高度如"800px"，注意应该有'px'单位
+                        var height_frame = document.getElementById("container").scrollHeight + 100;
+                        XN.CanvasClient.setCanvasHeight(height_frame + "px");
+                    });
+                    //生成浮动层
+                    $("<div id='floatblock'><ul><li><img src='" + Memento.info.uimg + "' /></li><li>" + Memento.info.uname + "</li><li class='light'>|</li><li>" + Memento.info.targetname + "</li><li><img src='" + Memento.info.targetimg + "' /></li></ul></div>").appendTo("#container");
+                    //隐藏输入框
+                    $(".input_area").hide();
+                    //显示“生成时间线”按钮
+                    $("#tools").show();
+                }
+            });
+        }
+        
+        $.when(getMyInfo, getFriendInfo).then(
+            getMemento, 
+            function(){
+                alert('用户信息异常');
             }
-        });
+        );
     });
     //绑定生成时间线按钮
     $("#MakeLine").bind("click", function() {
